ee2f353618ae831b387c17829cbef0ccc330957e
EpsOmega <adobewebs@gmail.com>
2026-06-15 03:44:28 -0400

    Delete hello-world.js

--- hello-world.js
+++
@@ -1,9 +0,0 @@
-class JimHelloWorldAddon extends HTMLElement {
-    constructor() {
-        super();
-    }
-    connectedCallback() {
-        alert("Hello World");
-    }
-}
-customElements.define('jim-hello-world', JimHelloWorldAddon);
